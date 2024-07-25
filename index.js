6d8a752c07357d03e84564fae299a5307722d283
Barbara Kälin <67426447+BarbWire-1@users.noreply.github.com>
2024-07-25 15:33:41 +0200

    built and included minified incl img support

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 import { ParticlesFactory } from './src/ParticlesFactory.js';
-const test = new ParticlesFactory({ particles: { shape: 'image', imageSrc: "./public/assets/images/sunflower.png" } });
-
+const test = new ParticlesFactory({
+	particles: {
+		shape: 'image',
+		imageSrc: "./public/assets/images/sunflower.png"
+	}
+});
 test.particles.imageSrc = "./public/assets/images/sunflower.png"
--- minified/particles-factory.es.js
+++ minified/particles-factory.es.js
@@ -1,17 +1,22 @@
-var it = Object.defineProperty;
-var T = (o) => {
+var st = Object.defineProperty;
+var B = (o) => {
   throw TypeError(o);
 };
-var st = (o, t, i) => t in o ? it(o, t, { enumerable: !0, configurable: !0, writable: !0, value: i }) : o[t] = i;
-var b = (o, t, i) => st(o, typeof t != "symbol" ? t + "" : t, i), I = (o, t, i) => t.has(o) || T("Cannot " + i);
-var l = (o, t, i) => (I(o, t, "read from private field"), i ? i.call(o) : t.get(o)), S = (o, t, i) => t.has(o) ? T("Cannot add the same private member more than once") : t instanceof WeakSet ? t.add(o) : t.set(o, i), y = (o, t, i, s) => (I(o, t, "write to private field"), s ? s.call(o, i) : t.set(o, i), i), r = (o, t, i) => (I(o, t, "access private method"), i);
-class et {
-  constructor(t, i, s, a, n, h) {
-    this.canvas = t, this.ctx = this.canvas.getContext("2d"), this.x = i, this.y = s, this.size = a, this.speed = n, this.fillStyle = h, this.updateSpeed(n);
+var et = (o, t, i) => t in o ? st(o, t, { enumerable: !0, configurable: !0, writable: !0, value: i }) : o[t] = i;
+var I = (o, t, i) => et(o, typeof t != "symbol" ? t + "" : t, i), D = (o, t, i) => t.has(o) || B("Cannot " + i);
+var l = (o, t, i) => (D(o, t, "read from private field"), i ? i.call(o) : t.get(o)), S = (o, t, i) => t.has(o) ? B("Cannot add the same private member more than once") : t instanceof WeakSet ? t.add(o) : t.set(o, i), f = (o, t, i, s) => (D(o, t, "write to private field"), s ? s.call(o, i) : t.set(o, i), i), r = (o, t, i) => (D(o, t, "access private method"), i);
+class at {
+  constructor(t, i, s, a, n, h, c = null) {
+    this.canvas = t, this.ctx = this.canvas.getContext("2d"), this.x = i, this.y = s, this.size = a, this.speed = n, this.fillStyle = h, this.imageSrc = c, c && this.loadImage(c), this.updateSpeed(n);
+  }
+  loadImage(t) {
+    this.image = new Image(), this.image.src = t, this.image.onload = () => {
+      this.imageLoaded = !0;
+    }, this.imageLoaded = !1;
   }
   drawParticle(t, i, s, a, n) {
     const h = this.ctx;
-    switch (n && (h.strokeStyle = n), h.fillStyle = t, h.globalAlpha = i, h.beginPath(), a) {
+    switch (n && (h.strokeStyle = n), h.globalAlpha = i, h.beginPath(), a) {
       case "circle":
         this.createCircle(h, s);
         break;
@@ -26,8 +31,14 @@ class et {
         break;
       case "triangle":
         this.createPolygon(h, s, 3, -Math.PI / 2, 1, "triangle");
+        break;
+      case "image":
+        this.imageLoaded && this.image && this.drawImage(h, s);
     }
     h.fill(), n && (h.strokeStyle = n, h.stroke());
+  }
+  drawImage(t, i) {
+    this.image && t.drawImage(this.image, this.x - i / 2, this.y - i / 2, i, i);
   }
   createCircle(t, i) {
     t.arc(this.x, this.y, i / 2, 0, 2 * Math.PI);
@@ -58,144 +69,143 @@ class et {
   handleMouseMove(t, i, s, a) {
     if (!+i) return;
     const n = t.clientX, h = t.clientY, { x: c, y: d } = this;
-    let m = n - s - c, f = h - a - d;
-    const u = Math.sqrt(m * m + f * f);
+    let g = n - s - c, p = h - a - d;
+    const u = Math.sqrt(g * g + p * p);
     if (u && i > u) {
-      m /= u, f /= u;
-      const v = 2;
-      this.x = c + m * -v, this.y = d + f * -v;
+      g /= u, p /= u;
+      const w = 2;
+      this.x = c + g * -w, this.y = d + p * -w;
     }
   }
 }
-var z, p, w, g, E, M, F, e, q, B, W, j, L, Y, H, N, O, X, A, R, G, J, K, Q, U, V, Z, k, $;
-const C = class C {
+var P, m, v, y, E, M, F, e, L, T, W, j, Y, H, N, O, X, G, R, q, J, K, Q, U, V, Z, $, b, _;
+const k = class k {
   constructor(t = { canvas: { id: "particles-canvas" } }) {
     S(this, e);
-    S(this, z);
-    S(this, p);
-    S(this, w);
-    S(this, g);
+    S(this, P);
+    S(this, m);
+    S(this, v);
+    S(this, y);
     S(this, E);
     S(this, M);
     S(this, F);
-    b(this, "getCanvasSize", () => {
-      const { width: t, height: i, prevDimensions: s } = r(this, e, N).call(this);
-      r(this, e, O).call(this, t, i), this.main.isResponsive && r(this, e, U).call(this, t, i, s);
+    I(this, "getCanvasSize", () => {
+      const { width: t, height: i, prevDimensions: s } = r(this, e, O).call(this);
+      r(this, e, X).call(this, t, i), this.main.isResponsive && r(this, e, V).call(this, t, i, s);
     });
     var s;
-    const i = C.defaultConfig;
+    const i = k.defaultConfig;
     for (const a in i) Object.preventExtensions(this[a] = { ...i[a], ...t[a] });
-    if (!this.canvas) throw new Error("To instantiate with the default settings you need at least to pass the canvas id like myParticles = new ParticlesFactory(canvas:{id: <your-canvas-id})");
     if (!this.particles.draw && !this.lines.draw) throw new Error("You need to define at least either a particles- or a lines-object to draw.");
-    y(this, p, []), y(this, M, ((s = this.particles) == null ? void 0 : s.size) || 2), y(this, F, r(this, e, Z).call(this, r(this, e, R))), r(this, e, q).call(this), r(this, e, B).call(this), r(this, e, A).call(this), r(this, e, k).call(this);
+    f(this, m, []), f(this, M, ((s = this.particles) == null ? void 0 : s.size) || 2), f(this, F, r(this, e, $).call(this, r(this, e, q))), r(this, e, L).call(this), r(this, e, T).call(this), r(this, e, R).call(this), r(this, e, b).call(this);
   }
   setFillMode(t) {
-    t === "noFill" ? (this.particles.noFill = !0, l(this, p).forEach((i) => i.fillStyle = "transparent")) : (this.particles.noFill = !1, t === "random" && (this.particles.randomFill = !0), t === "fill" && (this.particles.randomFill = !0, l(this, p).forEach((i) => i.fillStyle = this.particles.fillStyle)));
+    t === "noFill" ? (this.particles.noFill = !0, l(this, m).forEach((i) => i.fillStyle = "transparent")) : (this.particles.noFill = !1, t === "random" && (this.particles.randomFill = !0), t === "fill" && (this.particles.randomFill = !0, l(this, m).forEach((i) => i.fillStyle = this.particles.fillStyle)));
   }
   setSpeed(t) {
-    this.main.speed = t, l(this, p).forEach((i) => {
+    this.main.speed = t, l(this, m).forEach((i) => {
       i.updateSpeed(t);
     });
   }
   setNumParticles(t) {
     t = Math.round(t);
-    const i = l(this, p).length;
+    const i = l(this, m).length;
     let s = t - i;
-    t && s && s > 0 ? r(this, e, K).call(this, s) : r(this, e, Q).call(this, i, -s), this.main.numParticles = i + s;
+    t && s && s > 0 ? r(this, e, Q).call(this, s) : r(this, e, U).call(this, i, -s), this.main.numParticles = i + s;
   }
   setBaseSize(t) {
     const i = (t = Math.round(t)) / l(this, M);
-    l(this, p).forEach((s) => {
+    l(this, m).forEach((s) => {
       s.size *= i;
-    }), y(this, M, t);
+    }), f(this, M, t);
   }
   toggleFullScreen() {
     this.main.isFullScreen = !this.main.isFullScreen, this.getCanvasSize();
   }
   toggleAnimation() {
-    l(this, w) ? r(this, e, $).call(this) : r(this, e, k).call(this);
+    l(this, v) ? r(this, e, _).call(this) : r(this, e, b).call(this);
   }
 };
-z = new WeakMap(), p = new WeakMap(), w = new WeakMap(), g = new WeakMap(), E = new WeakMap(), M = new WeakMap(), F = new WeakMap(), e = new WeakSet(), q = function() {
-  this.canvasEl = document.getElementById(this.canvas.id), y(this, z, this.canvasEl.getContext("2d")), y(this, g, document.createElement("canvas")), y(this, E, l(this, g).getContext("2d")), this.getCanvasSize();
-}, B = function() {
+P = new WeakMap(), m = new WeakMap(), v = new WeakMap(), y = new WeakMap(), E = new WeakMap(), M = new WeakMap(), F = new WeakMap(), e = new WeakSet(), L = function() {
+  this.canvasEl = document.getElementById(this.canvas.id), f(this, P, this.canvasEl.getContext("2d")), f(this, y, document.createElement("canvas")), f(this, E, l(this, y).getContext("2d")), this.getCanvasSize();
+}, T = function() {
   const { x: t, y: i } = this.canvasEl.getBoundingClientRect();
   this.canvasEl.addEventListener("pointermove", (s) => {
-    l(this, p).forEach((a) => {
+    l(this, m).forEach((a) => {
       a.handleMouseMove(s, this.main.mouseDistance, t, i);
     });
   }), window.addEventListener("resize", () => {
-    this.main.isFullScreen && (this.getCanvasSize(), r(this, e, R).call(this));
+    this.main.isFullScreen && (this.getCanvasSize(), r(this, e, q).call(this));
   });
 }, W = function() {
   return "#" + (16777215 * Math.random() | 0).toString(16).padStart(6, "0");
 }, j = function(t, i, s) {
   return { x: Math.random() * (t - s / 2), y: Math.random() * (i - s / 2) };
-}, L = function(t) {
+}, Y = function(t) {
   return t * Math.max(0.2, Math.random());
-}, Y = function(t, i, s, a) {
+}, H = function(t, i, s, a) {
   const n = s - t, h = a - i;
   return Math.sqrt(n ** 2 + h ** 2);
-}, H = function(t, i) {
-  if (t && i) return r(this, e, Y).call(this, t.x, t.y, i.x, i.y);
-}, N = function() {
+}, N = function(t, i) {
+  if (t && i) return r(this, e, H).call(this, t.x, t.y, i.x, i.y);
+}, O = function() {
   const { innerWidth: t, innerHeight: i } = window, s = this.main.isFullScreen, a = i;
   return { width: s ? t : this.canvas.width, height: s ? a : this.canvas.height, prevDimensions: { width: this.canvasEl.width, height: this.canvasEl.height } };
-}, O = function(t, i) {
-  l(this, g).width = this.canvasEl.width = t, l(this, g).height = this.canvasEl.height = i;
-}, X = function() {
-  const { width: t, height: i } = l(this, g), { size: s, randomSize: a, fillStyle: n, randomFill: h, shape: c, draw: d } = this.particles;
-  let m = n, f = s;
-  d && (h && (m = r(this, e, W).call(this)), a && (f = r(this, e, L).call(this, s)));
-  const { x: u, y: v } = r(this, e, j).call(this, t, i, s);
-  return new et(l(this, g), u, v, f, this.main.speed, m, c);
-}, A = function(t = this.main.numParticles) {
-  for (; t; ) l(this, p).push(r(this, e, X).call(this)), t--;
-}, R = function() {
-  const t = this.main.numParticles, { draw: i, collision: s, randomFill: a, noFill: n, fillStyle: h, stroke: c, opacity: d, randomSize: m, size: f, shape: u } = this.particles, v = c ? this.lines.strokeStyle : void 0, P = l(this, E);
-  P.fillStyle = this.main.fillStyle, P.globalAlpha = 1, P.fillRect(0, 0, this.canvasEl.width, this.canvasEl.height), l(this, p).forEach((x) => x.updateCoords(this.particles.draw)), l(this, p).forEach((x, _) => {
-    if ((this.lines.draw && +this.lines.connectDistance || s) && r(this, e, V).call(this, x, _, t), i) {
-      let tt = n ? "transparent" : a ? x.fillStyle : h;
-      x.drawParticle(tt, d, m ? x.size : f, u, v);
-    }
-  }), r(this, e, G).call(this);
+}, X = function(t, i) {
+  l(this, y).width = this.canvasEl.width = t, l(this, y).height = this.canvasEl.height = i;
 }, G = function() {
-  l(this, z).drawImage(l(this, g), 0, 0);
-}, J = function(t, i, s, a) {
-  var m;
-  if (!i || !s || !((m = this.lines) != null && m.draw)) return;
+  const { width: t, height: i } = l(this, y), { size: s, randomSize: a, fillStyle: n, randomFill: h, shape: c, draw: d, imageSrc: g } = this.particles;
+  let p = n, u = s;
+  d && (h && (p = r(this, e, W).call(this)), a && (u = r(this, e, Y).call(this, s)));
+  const { x: w, y: z } = r(this, e, j).call(this, t, i, s);
+  return new at(l(this, y), w, z, u, this.main.speed, p, c === "image" ? g : null);
+}, R = function(t = this.main.numParticles) {
+  for (; t; ) l(this, m).push(r(this, e, G).call(this)), t--;
+}, q = function() {
+  const t = this.main.numParticles, { draw: i, collision: s, randomFill: a, noFill: n, fillStyle: h, stroke: c, opacity: d, randomSize: g, size: p, shape: u, imageSrc: w } = this.particles, z = c ? this.lines.strokeStyle : void 0, C = l(this, E);
+  C.fillStyle = this.main.fillStyle, C.globalAlpha = 1, C.fillRect(0, 0, this.canvasEl.width, this.canvasEl.height), l(this, m).forEach((x) => x.updateCoords(this.particles.draw)), l(this, m).forEach((x, tt) => {
+    if ((this.lines.draw && +this.lines.connectDistance || s) && r(this, e, Z).call(this, x, tt, t), i) {
+      let it = n ? "transparent" : a ? x.fillStyle : h;
+      x.drawParticle(it, d, g ? x.size : p, u, z);
+    }
+  }), r(this, e, J).call(this);
+}, J = function() {
+  l(this, P).drawImage(l(this, y), 0, 0);
+}, K = function(t, i, s, a) {
+  var g;
+  if (!i || !s || !((g = this.lines) != null && g.draw)) return;
   const { strokeStyle: n, lineWidth: h, opacity: c, connectDistance: d } = this.lines;
   if (d >= a) {
-    const { x: f, y: u } = i, { x: v, y: P } = s;
-    t.beginPath(), t.moveTo(f, u), t.lineTo(v, P), t.strokeStyle = n, t.lineWidth = h, t.globalAlpha = c, t.stroke();
+    const { x: p, y: u } = i, { x: w, y: z } = s;
+    t.beginPath(), t.moveTo(p, u), t.lineTo(w, z), t.strokeStyle = n, t.lineWidth = h, t.globalAlpha = c, t.stroke();
   }
-}, K = function(t) {
-  r(this, e, A).call(this, t);
-}, Q = function(t, i) {
-  l(this, p).splice(t - i, i), this.numParticles = l(this, p).length;
-}, U = function(t, i, s) {
+}, Q = function(t) {
+  r(this, e, R).call(this, t);
+}, U = function(t, i) {
+  l(this, m).splice(t - i, i), this.numParticles = l(this, m).length;
+}, V = function(t, i, s) {
   const a = t / s.width, n = i / s.height;
-  l(this, p).forEach((h) => {
+  l(this, m).forEach((h) => {
     h.x *= a, h.y *= n;
   });
-}, V = function(t, i, s) {
+}, Z = function(t, i, s) {
   var a, n;
   for (let h = i + 1; s > h; h++) {
-    const c = l(this, p)[h], d = r(this, e, H).call(this, t, c), { randomSize: m, size: f } = this.particles;
-    (a = this.lines) != null && a.draw && r(this, e, J).call(this, l(this, E), t, c, d), (n = this.particles) != null && n.collision && t.particlesCollision(m, f, t, c, d);
+    const c = l(this, m)[h], d = r(this, e, N).call(this, t, c), { randomSize: g, size: p } = this.particles;
+    (a = this.lines) != null && a.draw && r(this, e, K).call(this, l(this, E), t, c, d), (n = this.particles) != null && n.collision && t.particlesCollision(g, p, t, c, d);
   }
-}, Z = function(t) {
+}, $ = function(t) {
   let i;
   return function() {
     i || (t.apply(this, arguments), i = !0, setTimeout(() => i = !1, 1e3 / this.main.frameRate));
   };
-}, k = function() {
-  l(this, F).call(this), y(this, w, requestAnimationFrame(r(this, e, k).bind(this)));
-}, $ = function() {
-  cancelAnimationFrame(l(this, w)), y(this, w, null);
-}, b(C, "defaultConfig", { canvas: { id: "particles-canvas", width: 500, height: 500 }, main: { frameRate: 30, numParticles: 80, speed: 0.2, mouseDistance: 80, fillStyle: "#000", isFullScreen: !0, isResponsive: !0 }, particles: { shape: "triangle", fillStyle: "#ff0000", randomFill: !0, noFill: !0, stroke: !0, size: 44, randomSize: !0, draw: !0, collision: !1, opacity: 1 }, lines: { connectDistance: 100, strokeStyle: "#ffffff", draw: !0, lineWidth: 0.5, opacity: 1 } });
-let D = C;
-typeof window < "u" && (window.ParticlesFactory = D);
+}, b = function() {
+  l(this, F).call(this), f(this, v, requestAnimationFrame(r(this, e, b).bind(this)));
+}, _ = function() {
+  cancelAnimationFrame(l(this, v)), f(this, v, null);
+}, I(k, "defaultConfig", { canvas: { id: "particles-canvas", width: 500, height: 500 }, main: { frameRate: 30, numParticles: 80, speed: 0.2, mouseDistance: 80, fillStyle: "#000", isFullScreen: !0, isResponsive: !0 }, particles: { shape: "triangle", fillStyle: "#ff0000", randomFill: !0, noFill: !1, stroke: !0, size: 44, randomSize: !0, draw: !0, collision: !1, opacity: 1, imageSrc: null }, lines: { connectDistance: 100, strokeStyle: "#ffffff", draw: !0, lineWidth: 0.5, opacity: 1 } });
+let A = k;
+typeof window < "u" && (window.ParticlesFactory = A);
 export {
-  D as ParticlesFactory
+  A as ParticlesFactory
 };
